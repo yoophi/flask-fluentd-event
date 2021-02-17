c68d8015fb784ba7eaaa6bbfdacd660156d6082f
Pyunghyuk Yoo <yoophi@gmail.com>
2021-02-17 16:30:46 +0900

    :sparkles: ADD: basic functions for fluentd sender

--- flask_fluentd_event/__init__.py
+++ flask_fluentd_event/__init__.py
@@ -1,5 +1,60 @@
 """Top-level package for Flask Fluentd Event."""
 
 __author__ = """Pyunghyuk Yoo"""
-__email__ = 'yoophi@gmail.com'
-__version__ = '0.1.0'
+__email__ = "yoophi@gmail.com"
+__version__ = "0.1.0"
+
+import logging
+
+from six.moves.queue import Empty, Queue
+
+from fluent import sender
+
+
+class FluentdEvent(object):
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+            # Send events after every request finishes
+            app.after_request(self.send_events)
+
+        # Unbounded queue for sent events
+        self.queue = Queue()
+
+    def init_app(self, app):
+        tag_prefix = app.config.get("FLUENTD_EVENT_TAG_PREFIX", "flask.fluentd_event")
+        host = app.config.get("FLUENTD_EVENT_HOST", "localhost")
+        port = int(app.config.get("FLUENTD_EVENT_PORT", 24224))
+        self._sender = sender.FluentSender(tag_prefix, host=host, port=port)
+
+        # Use the newstyle teardown_appcontext if it's available,
+        # otherwise fall back to the request context
+        if hasattr(app, "teardown_appcontext"):
+            app.teardown_appcontext(self.send_events)
+        else:
+            app.teardown_request(self.send_events)
+
+    def event(self, tag, event):
+        self.queue.put((tag, event))
+
+    def send_events(self, exception):
+        """
+        Makes a best-effort to send all the events that it pushed during a
+        request but capable of missing some
+        """
+        pumping = True
+        while pumping:
+            try:
+                tag, event = self.queue.get_nowait()
+                self._sender.emit(tag, event)
+                self.queue.task_done()
+            except Empty:
+                pumping = False
+            except Exception as e:
+                # This is bad but it's worse to foul the request because
+                # of a logging issue
+                logging.exception(e)
+                self.queue.task_done()
+
+        return exception
--- setup.py
+++ setup.py
@@ -4,50 +4,52 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = [
+    "fluent-logger",
+    "six",
+]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Pyunghyuk Yoo",
-    author_email='yoophi@gmail.com',
-    python_requires='>=3.5',
+    author_email="yoophi@gmail.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Flask plugin to handle event with fluentd.",
-    entry_points={
-        'console_scripts': [
-            'flask_fluentd_event=flask_fluentd_event.cli:main',
-        ],
-    },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='flask_fluentd_event',
-    name='flask_fluentd_event',
-    packages=find_packages(include=['flask_fluentd_event', 'flask_fluentd_event.*']),
+    keywords="flask_fluentd_event",
+    name="flask_fluentd_event",
+    packages=find_packages(include=["flask_fluentd_event", "flask_fluentd_event.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/yoophi/flask_fluentd_event',
-    version='0.1.0',
+    url="https://github.com/yoophi/flask_fluentd_event",
+    version="0.1.0",
     zip_safe=False,
 )
